7430959e4eb9eb14a266a3c1ed831bc138a63336
CoderCrazy <35939420+CoderCrazy1@users.noreply.github.com>
2020-08-05 17:31:33 -0400

    Create Scientific Department.lua

---
+++ Scientific Department.lua
@@ -0,0 +1,16 @@
+return {
+	CharacterRespawn = function(Player, Character)
+		-- This function is fired every time a player's character is respawned on this team.
+	end,
+	Loadout = {
+	-- GroupId:GroupRank
+		["3507856:255,254,80,70,60,50,40,30,20,0"] = {
+			ShirtId = 5207242523, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5207235335, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"Clipboard"}, -- Tool names.
+			MorphName = "" -- Name of the morph. Leave blank if none.
+		},
+		
+		
+	}	
+}
