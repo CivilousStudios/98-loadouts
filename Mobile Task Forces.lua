e4e6fdf3cbc2cc48f796d73d46cf8edc3ec50a5a
CoderCrazy <35939420+CoderCrazy1@users.noreply.github.com>
2020-08-05 17:29:14 -0400

    Create Mobile Task Forces.lua

---
+++ Mobile Task Forces.lua
@@ -0,0 +1,167 @@
+return {
+	CharacterRespawn = function(Player, Character)
+		-- This function is fired every time a player's character is respawned on this team.
+	end,
+	Loadout = {
+		
+		--// Alpha - 1
+		["2888800:250"] = {
+			ShirtId = 5360140764, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5360178140, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"SCAR-H", "Glock-18", "Bandage", "Medkit", "Detain"}, -- Tool names.
+			MorphName = "MTF Alpha-1 TFL" -- Name of the morph. Leave blank if none.
+		},
+		
+		["2888800:30"] = {
+			ShirtId = 5360140764, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5360178140, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"SCAR-H", "Glock-18", "Bandage", "Medkit", "Detain"}, -- Tool names.
+			MorphName = "MTF Alpha-1" -- Name of the morph. Leave blank if none.
+		},
+		
+		["2888800:20"] = {
+			ShirtId = 5360140764, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5360178140, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"SCAR-L", "Glock-18", "Bandage", "Medkit", "Detain"}, -- Tool names.
+			MorphName = "MTF Alpha-1" -- Name of the morph. Leave blank if none.
+		},
+		
+		["2888800:1"] = {
+			ShirtId = 5360140764, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5360178140, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"SCAR-L", "Glock-18", "Bandage", "Medkit", "Detain"}, -- Tool names.
+			MorphName = "MTF Alpha-1" -- Name of the morph. Leave blank if none.
+		},
+		
+		--// Beta - 7
+		["2888788:250"] = {
+			ShirtId = 5212358538, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5212215480, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"M4", "Flamethrower", "Desert Eagle", "Bandage", "Medkit", "Detain"}, -- Tool names.
+			MorphName = "MTF Beta-7 TFL" -- Name of the morph. Leave blank if none.
+		},
+		
+		["2888788:30"] = {
+			ShirtId = 5212358538, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5212215480, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"M4", "Flamethrower", "Glock-18", "Bandage", "Medkit", "Detain"}, -- Tool names.
+			MorphName = "MTF Beta-7" -- Name of the morph. Leave blank if none.
+		},
+		
+		["2888788:30"] = {
+			ShirtId = 5212358538, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5212215480, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"M4", "Flamethrower", "Glock-18", "Bandage", "Medkit"}, -- Tool names.
+			MorphName = "MTF Beta-7" -- Name of the morph. Leave blank if none.
+		},
+		
+		["2888788:20"] = {
+			ShirtId = 5212358538, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5212215480, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"M4", "Flamethrower", "Glock-18", "Bandage", "Medkit"}, -- Tool names.
+			MorphName = "MTF Beta-7" -- Name of the morph. Leave blank if none.
+		},
+		
+		["2888788:1"] = {
+			ShirtId = 5212358538, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5212215480, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"M4", "Flamethrower", "Glock-18", "Bandage", "Medkit"}, -- Tool names.
+			MorphName = "MTF Beta-7" -- Name of the morph. Leave blank if none.
+		},
+		
+		--// Sigma - 9
+		["3010211:250"] = {
+			ShirtId = 5193212930, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5193093273, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"AUG A3", "Glock-18", "Bandage", "Medkit", "Detain"}, -- Tool names.
+			MorphName = "MTF Sigma-9 TFL" -- Name of the morph. Leave blank if none.
+		},
+		
+		["3010211:30"] = {
+			ShirtId = 5193212930, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5193093273, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"AUG A3", "Glock-18", "Bandage", "Medkit", "Detain"}, -- Tool names.
+			MorphName = "MTF Sigma-9" -- Name of the morph. Leave blank if none.
+		},
+		
+		["3010211:20"] = {
+			ShirtId = 5193212930, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5193093273, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"AUG A3", "Glock-18", "Bandage", "Medkit"}, -- Tool names.
+			MorphName = "MTF Sigma-9" -- Name of the morph. Leave blank if none.
+		},
+		
+		["3010211:1"] = {
+			ShirtId = 5193212930, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5193093273, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"AUG A3", "Glock-18", "Bandage", "Medkit"}, -- Tool names.
+			MorphName = "MTF Sigma-9" -- Name of the morph. Leave blank if none.
+		},
+		
+		--// Epsilon-11
+		["2888795:250"] = {
+			ShirtId = 5362227604, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5362228223, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"G36 Elite", "Glock-18", "Bandage", "Medkit", "Detain"}, -- Tool names.
+			MorphName = "MTF Epsilon-11 TFL" -- Name of the morph. Leave blank if none.
+		},
+		
+		["2888795:30"] = {
+			ShirtId = 5362227604, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5362228223, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"G36 Elite", "Glock-18", "Bandage", "Medkit", "Detain"}, -- Tool names.
+			MorphName = "MTF Epsilon-11" -- Name of the morph. Leave blank if none.
+		},
+		
+		["2888795:20"] = {
+			ShirtId = 5362227604, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5362228223, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"G36 Elite", "Glock-18", "Bandage", "Medkit"}, -- Tool names.
+			MorphName = "MTF Epsilon-11" -- Name of the morph. Leave blank if none.
+		},
+		
+		["2888795:1"] = {
+			ShirtId = 5362227604, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5362228223, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"G36 Elite", "Glock-18", "Bandage", "Medkit"}, -- Tool names.
+			MorphName = "MTF Epsilon-11" -- Name of the morph. Leave blank if none.
+		},
+		
+		--// Fallback default mtf group
+		
+		["3507861:255"] = {
+			ShirtId = 5360140764, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5360178140, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"SCAR-H", "Desert Eagle", "KSG 12", "Bandage", "Medkit", "Detain"}, -- Tool names.
+			MorphName = "MTF Directorate+" -- Name of the morph. Leave blank if none.
+		},
+		
+		["3507861:0"] = { -- Rank 0 is for SiD
+			ShirtId = 5360140764, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5360178140, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"SCAR-H", "Desert Eagle", "KSG 12", "Bandage", "Medkit", "Detain"}, -- Tool names.
+			MorphName = "MTF Directorate+" -- Name of the morph. Leave blank if none.
+		},
+		
+		["3507861:254"] = {
+			ShirtId = 5360140764, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5360178140, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"SCAR-H", "Desert Eagle", "KSG 12", "Bandage", "Medkit", "Detain"}, -- Tool names.
+			MorphName = "MTF Directorate+" -- Name of the morph. Leave blank if none.
+		},
+		
+		["3507861:20"] = {
+			ShirtId = 5360140764, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5360178140, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"P90 Elite", "Glock-17", "Bandage", "Medkit"}, -- Tool names.
+			MorphName = "MTF Directorate+" -- Name of the morph. Leave blank if none.
+		},
+		
+		["3507861:10"] = {
+			ShirtId = 5360140764, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 5360178140, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"P90 Elite", "Glock-17", "Bandage", "Medkit"}, -- Tool names.
+			MorphName = "MTF Directorate+" -- Name of the morph. Leave blank if none.
+		}
+	}	
+}
