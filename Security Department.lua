803ad19c732b74e4205ec64fb975b62081c38d4a
CoderCrazy <35939420+CoderCrazy1@users.noreply.github.com>
2020-08-17 12:17:15 -0400

    Create Security Department.lua

---
+++ Security Department.lua
@@ -0,0 +1,85 @@
+return {
+	CharacterRespawn = function(Player, Character)
+		-- This function is fired every time a player's character is respawned on this team.
+	end,
+	Loadout = {
+	-- GroupId:GroupRank
+		["3507860:255,254,140,130,0"] = {
+			ShirtId = 1161632544, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 1161640298, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"AUG A3", "MP5", "Desert Eagle", "Baton", "Detain", "Bandage"}, -- Tool names.
+			MorphName = "SD Deputy+" -- Name of the morph. Leave blank if none.
+		},
+		
+		["3507860:120,110"] = {
+			ShirtId = 1161632544, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 1161640298, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"AUG A3", "MP5", "Glock 18", "Baton", "Detain", "Bandage"}, -- Tool names.
+			MorphName = "SD Captain/Major" -- Name of the morph. Leave blank if none.
+		},
+		
+		
+		["6763175:50,40"] = {
+			ShirtId = 1161632544, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 1161640298, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"Combative Keycard", "Honey Badger", "Glock 18", "Baton", "Detain", "Bandage",}, -- Tool names.
+			MorphName = "SD MPL+" -- Name of the morph. Leave blank if none.		
+	    },
+		
+		["6763175:20,10"] = {
+			ShirtId = 1161632544, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 1161640298, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"Combative Keycard", "P90 Elite", "Glock 18", "Baton", "Detain"}, -- Tool names.
+			MorphName = "SD MP+" -- Name of the morph. Leave blank if none.	
+		},
+		
+		["6763171:50,30"] = {
+			ShirtId = 1161646678, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 1161647371, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"Combative Keycard", "AUG A3", "P90 Elite", "Glock 18", "Baton", "Detain", "Bandage"}, -- Tool names.
+			MorphName = "SD SCUL+" -- Name of the morph. Leave blank if none.	
+		},
+		
+		["6763171:20,10"] = {
+			ShirtId = 1161646678, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 1161647371, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"Combative Keycard", "P90 Elite", "Glock 18", "Baton", "Detain", "Bandage"}, -- Tool names.
+			MorphName = "SD SCU+" -- Name of the morph. Leave blank if none.		
+		},
+		
+		["7363692:40,30"] = {
+			ShirtId = 1161646678, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 1161647371, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"Combative Keycard", "Honey Badger", "SCAR-L", "Glock 18", "Baton", "Medkit", "Bandage"}, -- Tool names.
+			MorphName = "SD CML+" -- Name of the morph. Leave blank if none.		
+		},
+		
+		["7363692:20,10"] = {
+			ShirtId = 1161646678, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 1161647371, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"Combative Keycard", "M4", "Glock 18", "Baton", "Medkit", "Bandage"}, -- Tool names.
+			MorphName = "SD CM+" -- Name of the morph. Leave blank if none.		
+		},
+		
+		["3507860:100,90"] = {
+			ShirtId = 1161632544, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 1161640298, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"Combative Keycard", "P90 SOW", "Glock 18", "Baton", "Detain"}, -- Tool names.
+			MorphName = "SD Officer/Lieutenant" -- Name of the morph. Leave blank if none.
+		},
+		
+		["3507860:40,30"] = {
+			ShirtId = 1161632544, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 1161640298, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"Combative Keycard", "P90 Elite", "Glock 18", "Baton"}, -- Tool names.
+			MorphName = "SD Agent/Specialist" -- Name of the morph. Leave blank if none.		
+		},
+		
+		["3507860:20,10"] = {
+			ShirtId = 1161632544, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 1161640298, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"Combative Keycard", "P90", "Glock 18"}, -- Tool names.
+			MorphName = "SD Recruit/Guard" -- Name of the morph. Leave blank if none.		
+		}	
+	}	
+}
