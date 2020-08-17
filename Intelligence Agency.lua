04cf16ef6a12d0e07fa67ab9167f10e9be2faa4b
CoderCrazy <35939420+CoderCrazy1@users.noreply.github.com>
2020-08-17 12:16:14 -0400

    Create Intelligence Agency.lua

---
+++ Intelligence Agency.lua
@@ -0,0 +1,16 @@
+return {
+	CharacterRespawn = function(Player, Character)
+		-- This function is fired every time a player's character is respawned on this team.
+	end,
+	Loadout = {
+	-- GroupId:GroupRank
+		["3617863:80,70,50,30,20,10,0"] = {
+			ShirtId = 559097145, -- MUST BE THE IMAGE ID. Not the shirt id.
+			PantId = 559098341, -- MUST BE THE IMAGE ID. Not the pants id.
+			Loadout = {"Level 5 Keycard", "MP5", "Detain", "Bandage"}, -- Tool names.
+			MorphName = "" -- Name of the morph. Leave blank if none.
+		},
+		
+		
+	}	
+}
